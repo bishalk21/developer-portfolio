1b2ca44f6623ca82fd08cc13f5e3e14c4e745a4a
bishalk21 <karkibishal00@gmail.com>
2026-01-14 16:35:11 +1100

    ui updated and error fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Projects from "./pages/projects/Projects";
 import About from "./pages/about/About";
 import FooterComp from "./components/footer/FooterComp";
 import Navbar from "./components/header/Navbar";
-// import Services from "./components/services/Services";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
       <Technologies />
       <Projects />
       <About />
-      {/* <Services /> */}
       <FooterComp />
     </main>
   );
